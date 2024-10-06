4925dcf2ca88c40554e34146514c90e33a707ff8
jabeka <42553558+jabeka@users.noreply.github.com>
2024-10-06 18:08:20 +0200

    handle size on 2 bytes for struct up to 65535bytes instead of 255

--- EasyTransfer/src/EasyTransfer.cpp
+++ EasyTransfer/src/EasyTransfer.cpp
@@ -1,89 +1,103 @@
 #include "EasyTransfer.h"
 
+// Captures address and size of struct
+void EasyTransfer::begin(uint8_t *ptr, uint16_t length, Stream *theStream)
+{
+  rx_len = 0;
+  rx_array_inx = 0;
+  address = ptr;
+  size = length;
+  _stream = theStream;
 
-
-
-//Captures address and size of struct
-void EasyTransfer::begin(uint8_t * ptr, uint8_t length, Stream *theStream){
-	rx_len = 0;
-	rx_array_inx = 0;
-	address = ptr;
-	size = length;
-	_stream = theStream;
-	
-	//dynamic creation of rx parsing buffer in RAM
-	rx_buffer = (uint8_t*) malloc(size+1);
+  // dynamic creation of rx parsing buffer in RAM
+  rx_buffer = (uint8_t *)malloc(size + 4);
 }
 
-//Sends out struct in binary, with header, length info and checksum
-void EasyTransfer::sendData(){
-  uint8_t CS = size;
+// Sends out struct in binary, with header, length info and checksum
+void EasyTransfer::sendData()
+{
+  uint8_t CS = 0x00FF & size;
+  CS ^= (0xFF00 & size) >> 8;
   _stream->write(0x06);
   _stream->write(0x85);
-  _stream->write(size);
-  for(int i = 0; i<size; i++){
-    CS^=*(address+i);
-    _stream->write(*(address+i));
+  _stream->write((size & 0xFF00) >> 8);
+  _stream->write(size & 0x00FF);
+  for (int i = 0; i < size; i++)
+  {
+    CS ^= *(address + i);
+    _stream->write(*(address + i));
   }
   _stream->write(CS);
-
 }
 
-boolean EasyTransfer::receiveData(){
-  
-  //start off by looking for the header bytes. If they were already found in a previous call, skip it.
-  if(rx_len == 0){
-  //this size check may be redundant due to the size check below, but for now I'll leave it the way it is.
-    if(_stream->available() >= 3){
-	//this will block until a 0x06 is found or buffer size becomes less then 3.
-      while(_stream->read() != 0x06) {
-		//This will trash any preamble junk in the serial buffer
-		//but we need to make sure there is enough in the buffer to process while we trash the rest
-		//if the buffer becomes too empty, we will escape and try again on the next call
-		if(_stream->available() < 3)
-			return false;
-		}
-      if (_stream->read() == 0x85){
-        rx_len = _stream->read();
-		//make sure the binary structs on both Arduinos are the same size.
-        if(rx_len != size){
+boolean EasyTransfer::receiveData()
+{
+  // start off by looking for the header bytes. If they were already found in a previous call, skip it.
+  if (rx_len == 0)
+  {
+    // this size check may be redundant due to the size check below, but for now I'll leave it the way it is.
+    if (_stream->available() >= 3)
+    {
+      // this will block until a 0x06 is found or buffer size becomes less then 3.
+      while (_stream->read() != 0x06)
+      {
+        // This will trash any preamble junk in the serial buffer
+        // but we need to make sure there is enough in the buffer to process while we trash the rest
+        // if the buffer becomes too empty, we will escape and try again on the next call
+        if (_stream->available() < 3)
+          return false;
+      }
+      if (_stream->read() == 0x85)
+      {
+        auto len1 = _stream->read();
+        auto len2 = _stream->read();
+        rx_len = len1 << 8 | len2;
+        // make sure the binary structs on both Arduinos are the same size.
+        if (rx_len != size)
+        {
           rx_len = 0;
           return false;
         }
       }
     }
   }
-  
-  //we get here if we already found the header bytes, the struct size matched what we know, and now we are byte aligned.
-  if(rx_len != 0){
-    while(_stream->available() && rx_array_inx <= rx_len){
+
+  // we get here if we already found the header bytes, the struct size matched what we know, and now we are byte aligned.
+  if (rx_len != 0)
+  {
+    while (_stream->available() && rx_array_inx <= rx_len)
+    {
       rx_buffer[rx_array_inx++] = _stream->read();
     }
-    
-    if(rx_len == (rx_array_inx-1)){
-      //seem to have got whole message
-      //last uint8_t is CS
-      calc_CS = rx_len;
-      for (int i = 0; i<rx_len; i++){
-        calc_CS^=rx_buffer[i];
-      } 
-      
-      if(calc_CS == rx_buffer[rx_array_inx-1]){//CS good
-        memcpy(address,rx_buffer,size);
-		rx_len = 0;
-		rx_array_inx = 0;
-		return true;
-		}
-		
-	  else{
-	  //failed checksum, need to clear this out anyway
-		rx_len = 0;
-		rx_array_inx = 0;
-		return false;
-	  }
-        
+
+    if (rx_len == (rx_array_inx - 1))
+    {
+      // seem to have got whole message
+      // last uint8_t is CS
+      calc_CS = rx_len & 0x00FF;
+      calc_CS ^= ((rx_len & 0xFF00) >> 8);
+      for (int i = 0; i < rx_len; i++)
+      {
+        calc_CS ^= rx_buffer[i];
+      }
+
+      if (calc_CS == rx_buffer[rx_array_inx - 1])
+      { // CS good
+
+        memcpy(address, rx_buffer, size);
+        rx_len = 0;
+        rx_array_inx = 0;
+        return true;
+      }
+
+      else
+      {
+        // failed checksum, need to clear this out anyway
+        rx_len = 0;
+        rx_array_inx = 0;
+        return false;
+      }
     }
   }
-  
   return false;
 }
